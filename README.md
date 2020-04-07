87388ac6de5ce84bb13d4f2eadf3decf58a52f57
Leigh Halliday <leighhalliday@gmail.com>
2020-04-07 10:00:14 -0400

    Updating README showing how map and reduce work

--- README.md
+++ README.md
@@ -9,10 +9,10 @@ const { clusters, supercluster } = useSupercluster({
     -1.2411810957931664,
     52.61208435908725,
     -1.0083656811012531,
-    52.64495957533833
+    52.64495957533833,
   ],
   zoom: 12,
-  options: { radius: 75, maxZoom: 20 }
+  options: { radius: 75, maxZoom: 20 },
 });
 ```
 
@@ -39,3 +39,53 @@ Full instructions and an [example can be found here](https://www.leighhalliday.c
 ### Leaflet
 
 Full instructions and an [example can be found here](https://www.leighhalliday.com/leaflet-clustering).
+
+## Configuration
+
+The last (fourth) argument passed to the `useSupercluster` hook are options that are passed directly to the instance of Supercluster. You can use any of [Supercluster's options](https://github.com/mapbox/supercluster#options).
+
+### Map & Reduce Options
+
+As an example, you can use `map` and `reduce` to keep track of a total value summed up from across the points. In this case we have the total `cost`, the max `severity`, plus another `count` (which is redundant because Supercluster gives us the `point_count` property).
+
+```js
+const options = {
+  radius: 75,
+  maxZoom: 20,
+  map: (props) => ({
+    cost: props.cost,
+    severity: props.severity,
+    count: 1,
+  }),
+  reduce: (acc, props) => {
+    acc.count += 1;
+    acc.cost += props.cost;
+    acc.severity = Math.max(acc.severity, props.severity);
+    return acc;
+  },
+};
+```
+
+I found `map` and `reduce` a little confusing! The value returned from `map` of the first point is used as the initial value passed as the accumulator to the `reduce` function. The only `props` you have available in `reduce` are the ones returned from `map`. You technically don't need to return a value from `reduce` (it's not used), but instead need to mutate the accumulator object.
+
+Then these accumulated properties can be used and are available on each cluster:
+
+```jsx
+<ul>
+  {clusters.map((point) => {
+    const properties = point.properties || {};
+    if (properties.cluster) {
+      return (
+        <li key={point.id}>
+          <h2>Points: {properties.point_count}</h2>
+          <p>Cost: {properties.cost.toFixed(2)}</p>
+          <p>Severity: {properties.severity}</p>
+          <p>Count: {properties.count}</p>
+        </li>
+      );
+    } else {
+      return <li key={properties.crimeId}>{properties.category}</li>;
+    }
+  })}
+</ul>
+```
--- example/index.tsx
+++ example/index.tsx
@@ -11,58 +11,87 @@ const points: Array<Supercluster.PointFeature<GeoJsonProperties>> = [
     properties: {
       cluster: false,
       crimeId: 78212911,
-      category: "anti-social-behaviour"
+      category: "anti-social-behaviour",
+      severity: 1,
+      cost: 10.25,
     },
-    geometry: { type: "Point", coordinates: [-1.135171, 52.6376] }
+    geometry: { type: "Point", coordinates: [-1.135171, 52.6376] },
   },
   {
     type: "Feature",
     properties: {
       cluster: false,
       crimeId: 78213207,
-      category: "anti-social-behaviour"
+      category: "anti-social-behaviour",
+      severity: 3,
+      cost: 30.99,
     },
-    geometry: { type: "Point", coordinates: [-1.133005, 52.629835] }
+    geometry: { type: "Point", coordinates: [-1.133005, 52.629835] },
   },
   {
     type: "Feature",
     properties: {
       cluster: false,
       crimeId: 78213205,
-      category: "anti-social-behaviour"
+      category: "anti-social-behaviour",
+      severity: 2,
+      cost: 20.5,
     },
-    geometry: { type: "Point", coordinates: [-1.114732, 52.628909] }
+    geometry: { type: "Point", coordinates: [-1.114732, 52.628909] },
   },
   {
     type: "Feature",
     properties: {
       cluster: false,
       crimeId: 78213197,
-      category: "anti-social-behaviour"
+      category: "anti-social-behaviour",
+      severity: 1,
+      cost: 50.55,
     },
-    geometry: { type: "Point", coordinates: [-1.133691, 52.63625] }
-  }
+    geometry: { type: "Point", coordinates: [-1.133691, 52.63625] },
+  },
 ];
 
 const bounds: BBox = [
   -1.2411810957931664,
   52.61208435908725,
   -1.0083656811012531,
-  52.64495957533833
+  52.64495957533833,
 ];
 
 const zoom = 10;
-const options = { radius: 75, maxZoom: 20 };
+const options = {
+  radius: 75,
+  maxZoom: 20,
+  map: (props) => ({
+    cost: props.cost,
+    severity: props.severity,
+    count: 1,
+  }),
+  reduce: (acc, props) => {
+    acc.count += 1;
+    acc.cost += props.cost;
+    acc.severity = Math.max(acc.severity, props.severity);
+    return acc;
+  },
+};
 
 const App = () => {
   const { clusters } = useSupercluster({ points, bounds, zoom, options });
 
   return (
     <ul>
-      {clusters.map(point => {
+      {clusters.map((point) => {
         const properties = point.properties || {};
         if (properties.cluster) {
-          return <li key={point.id}>points: {properties.point_count}</li>;
+          return (
+            <li key={point.id}>
+              <h2>Points: {properties.point_count}</h2>
+              <p>Cost: {properties.cost.toFixed(2)}</p>
+              <p>Severity: {properties.severity}</p>
+              <p>Count: {properties.count}</p>
+            </li>
+          );
         } else {
           return <li key={properties.crimeId}>{properties.category}</li>;
         }
